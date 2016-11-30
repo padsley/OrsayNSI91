c9b67ad0de934c52d1cffc8da2f7700302620e7a
Philip Adsley <padsley@gmail.com>
2016-11-30 11:14:09 +0100

    Update sorter.cpp
    Needed to fix the paths so that it points to the right places. i.e. raw data in 'data' and sorted data in... uh... 'sorted'.

--- Midas2Root/sorter.cpp
+++ Midas2Root/sorter.cpp
@@ -2,8 +2,8 @@
   gROOT->ProcessLine(".L Midas2Root.C+");
   for(int i=154;i<=205;i++)
     {
-      gROOT->ProcessLine("Midas2Root(\"raw/\",\"sorted/\",i,0,false)");
-      gROOT->ProcessLine("Midas2Root(\"raw/\",\"sorted/\",i,0,true)");
+      gROOT->ProcessLine("Midas2Root(\"../data\",\"../sorted/\",i,0,false)");
+      gROOT->ProcessLine("Midas2Root(\"../data\",\"../sorted/\",i,0,true)");
     }
   gROOT->ProcessLine(".q");
 }
