a3c5f968935ae21ffbfbc99e58f7423384eb3591
splitpole <splitpole@ipnlintdm3.in2p3.fr>
2017-01-22 16:52:33 +0100

    Improved the shell script so it does not consume the original file and that it only sorts when it needs to

--- raw/transfer.sh
+++ raw/transfer.sh
@@ -3,4 +3,18 @@
 #This file should transfer everything new from the MIDAS computer and then unzip it.
 
 rsync -ravpt york@134.158.196.38:/TapeData/201701/* .
-gunzip *.gz
+#gzip -dk *.gz
+
+for i in `find . -name 'R*.gz'`
+do
+#echo $i
+j=${i%_*}
+#echo $j
+if [ ! -e $j ]
+then
+	echo $i
+	echo $j
+	gzip -d < $i > $j
+fi
+done
+
