726832db24e23e02a27cd3a2441a33df06d1844f
splitpole <splitpole@ipnlintdm3.in2p3.fr>
2017-01-24 17:02:25 +0100

    Adding modified alpha calibration code to repo

--- alpha_calibration/Calibration.cpp
+++ alpha_calibration/Calibration.cpp
@@ -9,27 +9,22 @@
 
 void Calibration()
 {
-  TFile *fin = TFile::Open("UncalibratedHistos_manual.root");
+  TFile *fin = TFile::Open("UncalibratedHistos.root");
   //fin->ls();
 
-  TH2F *ADCFronts2D = (TH2F*)fin->FindObjectAny("ADCFronts2D");
-  ADCFronts2D->Draw("col");
-
-  TH2F *ADCBacks2D = (TH2F*)fin->FindObjectAny("ADCBacks2D");
-  ADCBacks2D->Draw("col");
+  TH2F *ADCValues2D = (TH2F*)fin->FindObjectAny("ADCValues2D");
+  ADCValues2D->Draw("col");
   
-  double FrontGains[96], FrontOffsets[96], BackGains[96], BackOffsets[96];
-  for(int i=0;i<96;i++)
+  double Gains[192], Offsets[192];
+  for(int i=0;i<192;i++)
     {
-      FrontGains[i] = 0;
-      FrontOffsets[i] = 0;
-      BackGains[i] = 0;
-      BackOffsets[i] = 0;
+      Gains[i] = 0;
+      Offsets[i] = 0;
     }
   
-  for(int i=0;i<96;i++)
+  for(int i=0;i<192;i++)
     {
-      TH1D *hProj = ADCFronts2D->ProjectionX("_px",i+1,i+1,"");
+      TH1D *hProj = ADCValues2D->ProjectionX("_px",i+1,i+1,"");
       hProj->Draw();
 
       hProj->GetXaxis()->SetRangeUser(500,2500);
@@ -64,99 +59,21 @@ void Calibration()
 	  g->Draw("AL*");
 	  fit1->Draw("same");
 
-	  FrontOffsets[i] = fit1->GetParameter(0);
-	  FrontGains[i] = fit1->GetParameter(1);
+	  Offsets[i] = fit1->GetParameter(0);
+	  Gains[i] = fit1->GetParameter(1);
 	}
       else
 	{
-	  FrontOffsets[i] = 0;
-	  FrontGains[i] = 1;
-	  cout << "front fuck" << endl;
+	  Offsets[i] = 0;
+	  Gains[i] = 1;
+	  cout << "fuck" << endl;
 	}
     }
 
-   for(int i=0;i<96;i++)
-    {
-      TH1D *hProj = ADCBacks2D->ProjectionX("_px",i+1,i+1,"");
-      hProj->Draw();
-
-      hProj->GetXaxis()->SetRangeUser(500,2500);
-
-      TSpectrum *sp = new TSpectrum();
-      sp->Search(hProj,2,"",0.35);
-
-      if(sp->GetNPeaks()==3)
-	{
-	  float *chan = sp->GetPositionX();
-	  for(int n=0;n<2;n++)
-	    {
-	      for(int nn=n+1;nn<3;nn++)
-		{
-		  if(chan[nn]<chan[n])
-		    {
-		      float chan_tmp = chan[n];
-		      chan[n] = chan[nn];
-		      chan[nn] = chan_tmp;
-		    }
-		}
-	    }
-	  TGraph *g = new TGraph();
-	  g->SetName("fitting");
-	  double Ealpha[3] = {5.155,5.486,5.805};
-	  for(int n=0;n<3;n++)
-	    {
-	      g->SetPoint(n,chan[n],Ealpha[n]);
-	    }
-	  TF1 *fit1 = new TF1("fit1","[0]+[1]*x",0,5000);
-	  g->Fit("fit1","Q");
-	  g->Draw("AL*");
-	  fit1->Draw("same");
-
-	  BackOffsets[i] = fit1->GetParameter(0);
-	  BackGains[i] = fit1->GetParameter(1);
-	}
-      else
-	{
-	  BackOffsets[i] = 0;
-	  BackGains[i] = 1;
-	  cout << "back fuck" << endl;
-	}
-    }
-
-   ofstream output;
-   output.open("/home/padsley/codes/nptool/Projects/SPcoincW1_19Fpp/calibs/W1Calibs.txt");
-
-   for(int i=0;i<96;i++)
-     {
-       cout << "W1_D" << (i-i%16)/16+1 << "_FRONT" << i%16 << "_E\t" << FrontOffsets[i] << "\t" << FrontGains[i] << endl;
-       output << "W1_D" << (i-i%16)/16+1 << "_FRONT" << i%16 << "_E\t" << FrontOffsets[i] << "\t" << FrontGains[i] << endl;
-     }
-
-   for(int i=0;i<96;i++)
-     {
-       cout << "W1_D" << (i-i%16)/16+1 << "_BACK" << i%16 << "_E\t" << BackOffsets[i] << "\t" << BackGains[i] << endl;
-       output << "W1_D" << (i-i%16)/16+1 << "_BACK" << i%16 << "_E\t" << BackOffsets[i] << "\t" << BackGains[i] << endl;
-     }
-
-     output.close();
-     output.open("/home/padsley/data/19Fpp/W1Calibs.h");
+    ofstream output;
+    output.open("W1Calibs.h");
    
    output << "double CalibPars[192][2] = { ";
-   
-   double Gains[192], Offsets[192];
-   
-   for(int i=0;i<6;i++)
-   {
-       for(int j=0;j<16;j++)
-       {
-           cout << "32*(i-1)+j = " << 32*(i)+j << endl;
-            Offsets[32*(i)+j] = FrontOffsets[16*i+j];
-            Gains[32*(i)+j] = FrontGains[16*i+j];
-            
-            Offsets[32*(i)+j+16] = BackOffsets[16*i+j];
-            Gains[32*(i)+j+16] = BackGains[16*i+j];
-       }
-   }
    
    for(int i=0;i<191;i++)
    {
--- alpha_calibration/GenerateHistos.cpp
+++ alpha_calibration/GenerateHistos.cpp
@@ -8,68 +8,21 @@
 
 void GenerateHistos()
 {
-  TFile *f0 = TFile::Open("/home/padsley/data/19Fpp/sorted/R225_0.root");
-  TFile *f1 = TFile::Open("/home/padsley/data/19Fpp/sorted/R225_1.root");
-  TFile *f2 = TFile::Open("/home/padsley/data/19Fpp/sorted/R225_2.root");
+  TFile *f0 = TFile::Open("/home/splitpole/nsi91/sorted/R41_0_old.root");
 
-  TTree *t0 = (TTree*)f1->FindObjectAny("EGTree");
-  TTree *t1 = (TTree*)f1->FindObjectAny("EGTree");
-  TTree *t2 = (TTree*)f1->FindObjectAny("EGTree");
+  TTree *t0 = (TTree*)f0->FindObjectAny("EGTree");
+  t0->Print();
 
   TChain *ch = new TChain("EGTree");
-  ch->Add("/home/padsley/data/19Fpp/sorted/R225_0.root");
-  ch->Add("/home/padsley/data/19Fpp/sorted/R225_1.root");
-  ch->Add("/home/padsley/data/19Fpp/sorted/R225_2.root");
+  ch->Add("/home/splitpole/nsi91/sorted/R41_0_old.root");
 
-  TH1F **ADCFronts = new TH1F*[16*6];
-  TH1F **ADCBacks  = new TH1F*[16*6];
+  TH2F *ADCValues2D = new TH2F("ADCValues2D","Two-dimensional plot of ADC values from front strips",4096,0,4096,32*6,0,32*6);
 
-  TH2F *ADCFronts2D = new TH2F("ADCFronts2D","Two-dimensional plot of ADC values from front strips",4096,0,4096,16*6,0,16*6);
-  TH2F *ADCBacks2D = new TH2F("ADCBacks2D","Two-dimensional plot of ADC values from back strips",4096,0,4096,16*6,0,16*6);
+  t0->Draw("adcList:adcData>>ADCValues2D","","col");
   
-  for(int i=0;i<16*6;i++)
-    {
-      char buffer[256], buffer2[256];
-      sprintf(buffer,"FrontStrip%d",i);
-      sprintf(buffer2,"ADC spectrum for Front Strip %d",i);
-      //cout << buffer << endl;
-      ADCFronts[i] = new TH1F(buffer,buffer2,4096,0,4096);
-      sprintf(buffer,"BackStrip%d",i);
-      sprintf(buffer2,"ADC spectrum for Back Strip %d",i);
-      ADCBacks[i] = new TH1F(buffer,buffer2,4096,0,4096);
-    }
-
 
   TFile *fout = new TFile("UncalibratedHistos.root","RECREATE");
-
-  char buffer[256], buffer2[256];
-  sprintf(buffer,"16*(fW1_FrontE_DetectorNbr-1)+fW1_FrontE_StripNbr:fW1_FrontE_Energy>>ADCFronts2D(4096,0,4096,96,0,96)");
-  ch->Draw(buffer,"","col");
-
-  sprintf(buffer,"16*(fW1_BackE_DetectorNbr-1)+fW1_BackE_StripNbr:fW1_BackE_Energy>>ADCBacks2D(4096,0,4096,96,0,96)");
-  ch->Draw(buffer,"","col");
-  
-  for(int i=0;i<6;i++)
-    {
-      for(int j=0;j<16;j++)
-	{
-	  //cout << "i: " << i << "\t j: " << j << endl;
-	  //char buffer[256], buffer2[256];
-	  //sprintf(buffer,"fW1_FrontE_Energy>>ADCFronts[%d]",16*i+j);
-	  //sprintf(buffer2,"fW1_FrontE_DetectorNbr==%d && fW1_FrontE_StripNbr==%d",i,j);
-	  //t1->Draw(buffer,buffer2,"");
-	  //ADCFronts[16*i+j]->Write();
-
-	  //sprintf(buffer,"fW1_BackE_Energy>>ADCBacks[%d]",16*i+j);
-	  //sprintf(buffer2,"fW1_BackE_DetectorNbr==%d && fW1_BackE_StripNbr==%d",i,j);
-	  //t1->Draw(buffer,buffer2,"");
-	  //ADCBacks[16*i+j]->Write();
-	}
-    }
-
-  ADCFronts2D->Write();
-  ADCBacks2D->Write();
-  
+  ADCValues2D->Write();
   fout->Close();
 }
 	  
